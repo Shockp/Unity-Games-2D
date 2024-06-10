c8431f3b3c6aa8235e6a53b71975ad95932537a3
Shockp <59491201+Shockp@users.noreply.github.com>
2024-06-10 03:00:52 +0200

    Update CameraController.cs

--- Legend of Eldoria (RPG)  /Camera /CameraController.cs
+++ Legend of Eldoria (RPG)  /Camera /CameraController.cs
@@ -1,1 +1,10 @@
-//
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+
+    private void LateUpdate() {
+        this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+    }
+}
